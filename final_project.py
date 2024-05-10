baa064ae19a0dcae838c9d6b979ba05e5fde06bb
Andrei Balint <toncara71@gmail.com>
2024-05-10 18:16:20 +0300

    first_push

---
+++ final_project.py
@@ -0,0 +1,7 @@
+class Contact:
+    def __init__(self, name, address, phone, email, birthday):
+        self.name = name
+        self.address = address
+        self.phone = phone
+        self.email = email
+        self.birthday = birthday
